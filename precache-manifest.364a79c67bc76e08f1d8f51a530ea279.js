36babd16c245163ea7f51450e7771d9f52576a3e
korn <kornjiradej@gmail.com>
2019-07-15 14:37:41 +0700

    Updates

---
+++ precache-manifest.364a79c67bc76e08f1d8f51a530ea279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955e958c6cfe8528e38d8fe890bc781c",
+    "url": "/workpoint-tv-gh-pages/index.html"
+  },
+  {
+    "revision": "b9114aac1c94dff2bc3c",
+    "url": "/workpoint-tv-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/workpoint-tv-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b9114aac1c94dff2bc3c",
+    "url": "/workpoint-tv-gh-pages/static/js/main.b1039712.chunk.js"
+  },
+  {
+    "revision": "40a9d7e449bccab58508",
+    "url": "/workpoint-tv-gh-pages/static/js/runtime~main.8f873713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workpoint-tv-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
